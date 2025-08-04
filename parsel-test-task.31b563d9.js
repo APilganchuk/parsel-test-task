384276b720886e904a0c27c2656e8fc40334bd3f
APilganchuk <opilganchuk@gmail.com>
2025-08-04 13:57:30 +0300

    Updates

--- parsel-test-task.31b563d9.js
+++ parsel-test-task.31b563d9.js
@@ -160,11 +160,11 @@
       });
     }
   }
-})({"arM2V":[function(require,module,exports,__globalThis) {
+})({"5j6Kf":[function(require,module,exports,__globalThis) {
 var global = arguments[3];
 var HMR_HOST = null;
 var HMR_PORT = null;
-var HMR_SERVER_PORT = 50324;
+var HMR_SERVER_PORT = 1234;
 var HMR_SECURE = false;
 var HMR_ENV_HASH = "439701173a9199ea";
 var HMR_USE_SSE = false;
@@ -758,6 +758,6 @@ exports.export = function(dest, destName, get) {
     });
 };
 
-},{}]},["arM2V","a0t4e"], "a0t4e", "parcelRequirebd51", {})
+},{}]},["5j6Kf","a0t4e"], "a0t4e", "parcelRequirebd51", {})
 
 //# sourceMappingURL=parsel-test-task.31b563d9.js.map
